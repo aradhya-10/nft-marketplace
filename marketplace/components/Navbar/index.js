e7572a57c1f4b8be211faff4aeb9ced35e80019f
vampire-ab <brijeshagal@gmail.com>
2022-11-28 11:05:24 +0530

    Base

---
+++ marketplace/components/Navbar/index.js
@@ -0,0 +1,7 @@
+import Discover from "./Discover";
+import HelpCenter from "./HelpCenter"
+import Notification from "./Notification";
+import Profile from "./Profile";
+import SideBar from "./SideBar";
+
+export {Discover, HelpCenter, Profile, SideBar, Notification};
