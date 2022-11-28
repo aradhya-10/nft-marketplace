5bcd85cd64f0724521f5a681c3b12088da55627e
aradhya-10 <202051034@iiitvadodara.ac.in>
2022-11-29 00:18:31 +0530

    Add footer jsx

--- marketplace/pages/_app.js
+++ marketplace/pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import { Navbar } from "../components/componentsIndex";
+import { Footer } from "../components/componentsIndex";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Navbar />
       <Component {...pageProps} />
+	  <Footer />
     </div>
   );
 }
